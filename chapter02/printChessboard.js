84069b24d5d8eb766b9c96e10848925b6932fd59
Leonardo Toccaceli <leotoccaceli@gmail.com>
2020-05-29 10:57:29 -0300

    changed parameter 'lines' name to 'columns'

--- chapter02/printChessboard.js
+++ chapter02/printChessboard.js
@@ -1,22 +1,22 @@
-function printBoard(rows, lines) {
+function printBoard(rows, columns) {
   let arr = [];
 
   for (let i = 0; i < rows; i++) {
     if (i % 2 === 0) {
-      for (let j = 0; j <= lines; j++) {
-        if (j !== lines && j % 2 === 0) {
+      for (let j = 0; j <= columns; j++) {
+        if (j !== columns && j % 2 === 0) {
           arr.push(" ");
-        } else if (j !== lines && j % 2 !== 0) {
+        } else if (j !== columns && j % 2 !== 0) {
           arr.push("#");
         } else {
           arr.push("\r\n");
         }
       }
     } else if (i % 2 !== 0) {
-      for (let j = 0; j <= lines; j++) {
-        if (j !== lines && j % 2 === 0) {
+      for (let j = 0; j <= columns; j++) {
+        if (j !== columns && j % 2 === 0) {
           arr.push("#");
-        } else if (j !== lines && j % 2 !== 0) {
+        } else if (j !== columns && j % 2 !== 0) {
           arr.push(" ");
         } else {
           arr.push("\r\n");
